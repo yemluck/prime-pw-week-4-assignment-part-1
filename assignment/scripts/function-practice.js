f6d2ac5db01abfaf61abe4854ba1ae148f0a70f8
Chris Agbolade <yemluck@gmail.com>
2021-11-22 16:08:33 -0600

    Question 7 fixed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,8 +15,8 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  let output = 'Hello ' + name + '!'
-  return output;
+  return 'Hello ' + name + '!';
+  
 }
 // Remember to call the function to test
 console.log('Running function helloName with my name:', helloName("Chris"));
@@ -55,47 +55,61 @@ Is this a better synthax than the above. I know you said simple is better
 */
 
 
-// 5. Function that will return true if a number is positive, 
+// 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
+  let output;
   if ( number > 0 ){
-    return true;
+    output = true;
   } else {
-    return false;
+    output = false;
   }
+  return output;
 }
 
-// Call the function to test each outcome (true & false) 
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log( '3 is a positive integer?', isPositive(3) );
 console.log( '0 is a positive integer?', isPositive(0) );
 console.log( 'Negative 3 is a positive integer', isPositive(-3) );
 
 
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
+let array = ['coke', 'pepsi', 'fanta', 'lime', 'sprite']
+
 function getLast( array ) {
-  //let array = [];  ----> I thought I needed to declare array but got error message that array has already been declared
+  let output = 'undefined';
+  //let array = [];  ----> I thought I needed to declare array in the function but got error message that array has already been declared
+  // so I had to declare it outside the function
   if (array.length === 0){
-    return 'undefined'
+    output = 'undefined'
   } else {
-    return array[array.length - 1];
+    output = array[array.length - 1];
   }
+  return output;
 } // end getLast
-console.log('The last item in the soda array is', getLast(['coke', 'pepsi', 'fanta', 'lime', 'sprite']));
+console.log('The last item in the soda array is', getLast(array));
 
 
-// 7. Function to find a value in an array. Return true if the 
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-  
+  for (let x of array){
+    if(x === value){
+      return true;
+    } 
+  }
+  return false;
 }
+console.log('Is the value in the array? Expect True', find(2, [1,2,3]));
+console.log('Is the value in the array? expect false', find(5, [1,2,3]));
 
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
 
@@ -104,11 +118,16 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array2) {
   let sum = 0
+  for (x of array2){
+    sum += x;
+  }
   // TODO: loop to add items
   return sum;
 }
+console.log('The numbers in the array add up to:', sumAll([1,2,3]))
+console.log(sumAll([4,7,9,5]))
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
@@ -116,6 +135,6 @@ function sumAll( ) {
 
 
 
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
